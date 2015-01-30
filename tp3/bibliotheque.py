9f2f01da3ec292d9c70a78bda7d3df38fae8fc3a
Kevin <kev_gti@hotmail.fr>
2015-01-30 15:48:54 +0100

    ajout TP3

--- tp3/bibliotheque.py
+++ tp3/bibliotheque.py
@@ -1,2 +1,24 @@
 # SVL 30/01/15
 # ex emprunts des livres
+
+"""
+Service des emprunts de la bibliothèque
+
+Comportements a tester :
+- cas nominal
+- cas d'exception : le membre a atteint son quota
+- cas d'exception : le livre n'est que consultable
+"""
+class ServiceEmprunt():
+	
+	def __init__(self, lesemprunts):
+		self.emprunts = lesemprunts
+
+	def emprunter(self, livre, carte):
+		if not livre.est_empruntable():
+			raise LivreNonEmpruntableError()
+		self.emprunts.ajouterEmprunt(livre, carte)
+
+class LivreNonEmpruntableError(Exception):
+	print("Le livre n'est pas empruntable")
+	
--- tp3/test_bibliotheque.py
+++ tp3/test_bibliotheque.py
@@ -1,14 +1,29 @@
 import unittest
 from mockito import *
+from bibliotheque import *
 
 class TesterUnEmprunt(unittest.TestCase):
 
-    def tester_cas_nominal_enprunt_reussi(self):
-        stockageEmprunt = mock()
+	def setUp(self):
+		self.stockageEmprunt = mock()
+		self.livre = mock()
+		self.carte = mock()
+		self.service = ServiceEmprunt(self.stockageEmprunt)
 
-        service = ServiceEmprunt(stockageEmprunt)
-        service.emprunter(livre, carte)
+	def tester_cas_nominal_enprunt_reussi(self):
+		when(self.livre).est_empruntable().thenReturn(True)
 
-        verify(stockageEmprunt).ajouterEmprunt(livre, carte)
+		# emprunt = mock()
+		# when (stockageEmprunt).ajouterEmprunt(livre, carte).thenReturn(emprunt)
+
+		resultat = self.service.emprunter(self.livre, self.carte)
+
+		verify(self.stockageEmprunt).ajouterEmprunt(self.livre, self.carte)
+
+        # assert_equal(resultat, emprunt)
 
 
+	def tester_emprunt_echoue_car_livre_non_empruntable(self):
+		when(self.livre).est_empruntable().thenReturn(False)
+
+		self.assertRaises(LivreNonEmpruntableError, self.service.emprunter, self.livre, self.carte)
